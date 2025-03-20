865f0e9e03f3fbafafd9d5f92689c5e6e5430918
vatsal prajapati <vatsalprajapati@gmail.com>
2025-03-20 09:14:53 +0530

    selling price and costing price program

---
+++ spcp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int cp;
+    cout << "Enter Cost Price ";
+    cin >> cp;
+    int sp;
+    cout << "Enter Selling Price ";
+    cin >> sp;
+    if (sp > cp)
+    {
+        cout << "Profit is " << sp - cp;
+    }
+    else if (sp < cp)
+    {
+        cout << "Loss is " << cp - sp;
+    }
+    else
+    {
+        cout << "No Profit No Loss";
+    }
+}
