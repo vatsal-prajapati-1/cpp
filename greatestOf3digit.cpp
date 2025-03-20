9acace242f09cc82d357d2c0bad5b835069b9ef0
vatsal prajapati <vatsalprajapati@gmail.com>
2025-03-20 21:43:30 +0530

    greatest of 3 digit program

---
+++ greatestOf3digit.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "Enter the first number : ";
+    cin >> a;
+    cout << "Enter the second number : ";
+    cin >> b;
+    cout << "Enter the third number : ";
+    cin >> c;
+    if (a > b && a > c)
+    {
+        cout << a << " is greatest number" << endl;
+    }
+    else if (b > a && b > c)
+    {
+        cout << b << " is greatest number" << endl;
+    }
+    else
+    {
+        cout << c << " is greatest number" << endl;
+    }
+}
