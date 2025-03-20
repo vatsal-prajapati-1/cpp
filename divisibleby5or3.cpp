399413f1ee54bb35680916001b4118b7338b0272
vatsal prajapati <vatsalprajapati@gmail.com>
2025-03-20 21:51:43 +0530

    divisible by 5 or 3 program

---
+++ divisibleby5or3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    if (n % 5 == 0 || n % 3 == 0)
+    {
+        cout << "Divisible by 5 or 3" << endl;
+    }
+    else
+    {
+        cout << "Not Divisible by 5 and 3" << endl;
+    }
+}
