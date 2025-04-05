a6e9dc97eaa5909cea879e18690769bd5e7f8c8f
vatsal prajapati <vatsalprajapati@gmail.com>
2025-04-05 11:24:27 +0530

    numeric square program

---
+++ numericSquare.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter side of square : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter side of number : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
