0d15ab4a316719b041458e6f2f72f49c9f656832
vatsal prajapati <vatsalprajapati@gmail.com>
2025-04-05 11:21:10 +0530

    star pyramid program

---
+++ starPyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int g = 1; g <= 2 * i - 1; g++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
