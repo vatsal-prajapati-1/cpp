93bb767c27301282e977ac29e7ccedfb88ad505b
vatsal prajapati <vatsalprajapati@gmail.com>
2025-04-05 11:15:36 +0530

    numeric and alphabatic program

---
+++ numericAndAlphabatic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (i % 2 != 0)
+                cout << j << " ";
+            else
+                cout << (char)(j + 64) << " ";
+        }
+        cout << endl;
+    }
+}
