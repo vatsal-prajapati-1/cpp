0df02c8be8682e757e85aca07364fc606b388975
vatsal prajapati <vatsalprajapati@gmail.com>
2025-04-06 15:49:24 +0530

    combination program

---
+++ combination.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int fact(int x)
+{
+    int f = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
+int main()
+{
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int r;
+    cout << "Enter r : ";
+    cin >> r;
+    int a = fact(n);
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     a *= i;
+    // }
+    int b = fact(r);
+    // for (int i = 1; i <= r; i++)
+    // {
+    //     b *= i;
+    // }
+    int c = fact(n - r);
+    // for (int i = 1; i <= n - r; i++)
+    // {
+    //     c *= i;
+    // }
+    cout << a / (b * c);
+}
