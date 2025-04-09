6c38190de14e1b3182be68fcb33bfc2fc3316074
vatsal prajapati <vatsalprajapati@gmail.com>
2025-04-09 19:13:52 +0530

    square of natural number program

---
+++ squareOfNaturalNumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the element of the array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] * arr[j] << " ";
+    }
+}
