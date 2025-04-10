43862b99730c209fadbdaaf252c3ccc1fe4889b0
vatsal prajapati <vatsalprajapati@gmail.com>
2025-04-10 08:49:06 +0530

    odd and even index replace by a program

---
+++ oddAndEvenString.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cout << "Enter a string : ";
+    getline(cin, s);
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (i % 2 == 0)
+            s[i] = 'a';
+    }
+    cout << s;
+}
